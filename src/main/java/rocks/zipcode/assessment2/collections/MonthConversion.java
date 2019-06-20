973c5ddb3c6df366654edc9c280b1dc6e7d674d0
donnasvinis <dvsvinis@mac.com>
2019-06-20 07:44:10 -0400

    Fixed testGetNumber whenNotInThere test

--- src/main/java/rocks/zipcode/assessment2/collections/MonthConversion.java
+++ src/main/java/rocks/zipcode/assessment2/collections/MonthConversion.java
@@ -1,19 +1,25 @@
 package rocks.zipcode.assessment2.collections;
 
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Use a map to solve
  */
 public class MonthConversion {
-    private String monthName;
-    private Integer monthNumber;
+    Map<Integer,String> hashMap;
+
+    public MonthConversion() {
+        this.hashMap = new HashMap<>();
+    }
 
     /**
      * @param monthNumber - ordinal of month in the year; i.e. January = 1, February = 2
      * @param monthName - name of month
      */
     public void add(Integer monthNumber, String monthName) {
-        this.monthNumber = monthNumber;
-        this.monthName = monthName;
+        hashMap.put(monthNumber, monthName);
     }
 
     /**
@@ -21,30 +27,21 @@ public class MonthConversion {
      * @return the name of the respective month
      */
     public String getName(Integer monthNumber) throws NullPointerException{
-        String result = "";
-        if (monthNumber == 1) result = "January";
-        if (monthNumber == 2) result = "February";
-        if (monthNumber == 3) result = "March";
-        if (monthNumber == 4) result = "April";
-        if (monthNumber == 5) result = "May";
-        if (monthNumber == 6) result = "June";
-        if (monthNumber == 7) result = "July";
-        if (monthNumber == 8) result = "August";
-        if (monthNumber == 9) result = "September";
-        if (monthNumber == 10) result = "October";
-        if (monthNumber == 11) result = "November";
-        if (monthNumber == 12) result = "December";
-        return result;
-  //      throw new NullPointerException();
+     return hashMap.get(monthNumber);
     }
 
     /**
      * @param monthName - name of month
      * @return - the ordinal of the month in the year
      */
-    public int getNumber(String monthName) {
-
-        return (Integer)null;
+    public Integer getNumber(String monthName){
+        Integer result = null;
+        for (int i = 1; i <= 12; i++) {
+           if (hashMap.get(i) == monthName){
+               result = i;
+           }
+        }
+        return result;
     }
 
     /**
@@ -53,7 +50,7 @@ public class MonthConversion {
      */
     public Boolean isValidNumber(Integer monthNumber) {
 
-        return null;
+        return hashMap.containsKey(monthNumber);
     }
 
     /**
@@ -62,7 +59,7 @@ public class MonthConversion {
      */
     public Boolean isValidMonth(String monthName) {
 
-        return null;
+        return hashMap.containsValue(monthName);
     }
 
     /**
@@ -70,7 +67,7 @@ public class MonthConversion {
      */
     public Integer size() {
 
-        return -1;
+        return hashMap.size();
     }
 
     /**
@@ -78,6 +75,7 @@ public class MonthConversion {
      * @param monthName - name of month
      */
     public void update(Integer monthNumber, String monthName) {
+        hashMap.replace(monthNumber, monthName);
 
     }
 
--- src/main/java/rocks/zipcode/assessment2/generics/Shelter.java
+++ src/main/java/rocks/zipcode/assessment2/generics/Shelter.java
@@ -3,6 +3,10 @@ package rocks.zipcode.assessment2.generics;
 
 import rocks.zipcode.assessment2.generics.ageable.Ageable;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 /**
  * Shelter is a generic class that holds Objects that extends `Ageable`.
  * For example, if a Person and a Dog extends Ageable, then I can declare the following:
@@ -10,33 +14,48 @@ import rocks.zipcode.assessment2.generics.ageable.Ageable;
  * Shelter<Person> farmHouse = new Shelter<Person>();
  * Shelter<Dog> dogHouse = new Shelter<Dog>();
  */
-public class Shelter<_> extends Ageable {
+public class Shelter<Occupant> extends Ageable implements Iterable{
+
+    private List<Occupant> occupants;
+
+
     public Shelter() {
-        throw new NullPointerException();
+        this.occupants = new ArrayList<>();
+ //       throw new NullPointerException();
     }
 
     /**
      * @return the number of item in the shelter
      */
     public int size() {
-        return -1;
+        return occupants.size();
     }
 
-    public void add(Object object) {
+    public void add(Occupant occupant) {
+        occupants.add(occupant);
     }
 
-    public Boolean contains(Object object) {
+    public Boolean contains(Occupant occupant) {
+
+        return occupants.contains(occupant);
+    }
+
+    public void remove(Occupant occupant) {
+        occupants.remove(occupant);
+    }
+
+    public Occupant get(Integer index) {
+
+        return occupants.get(index);
+    }
+
+    public Integer getIndexOf(Occupant ageable) {
+
+        return occupants.indexOf(ageable);
+    }
+
+    @Override
+    public Iterator iterator() {
         return null;
-    }
-
-    public void remove(Object object) {
-    }
-
-    public Object get(Integer index) {
-        return null;
-    }
-
-    public Integer getIndexOf(Object ageable) {
-        return 0;
     }
 }
